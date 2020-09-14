92607a7f2b6b49a51dbefc03cfcfbd10255177f5
snreynoso-T480 <snreynoso@gmail.com>
2020-09-14 20:15:20 -0300

    Search display suggest

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 var myApp = myApp || {};
 var gifoSearchArray = [];
 var numGifSearch = 24;
+var numGifSuggest = 4;
 
 const GIPHY_KEY = '2256WmPycPBqyzJaDx6Gzx0IoZspB7Ml';
 
 // SEARCH FUNCTIONS
-async function gifoSearch(keyword, GIPHY_KEY) {
+async function gifoSearch(keyword) {
     let url = `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${GIPHY_KEY}&limit=${numGifSearch}`;
 
     let response = await fetch(url);
@@ -63,16 +64,14 @@ function displaySearch(gifsPromise, keyword) {
 }
 
 // TRENDING FUNCTIONS
-async function gifoTrending(GIPHY_KEY) {
+async function gifoTrending() {
     let url = `http://api.giphy.com/v1/gifs/trending?&api_key=${GIPHY_KEY}&limit=3`;
-
     let response = await fetch(url);
     let gifsPromise = await response.json();
-
     return gifsPromise;
 }
 
-function displayTrending(gifsPromise){
+function displayTrending(gifsPromise) {
     let cont = 0;
     var gifHTML = [];
 
@@ -105,6 +104,132 @@ function displayTrending(gifsPromise){
     cont = 0;
 }
 
+async function gifoSuggest(keycode) {
+    let url = `http://api.giphy.com/v1/gifs/search/tags?&api_key=${GIPHY_KEY}&q=${keycode}&limit=${numGifSuggest}`;
+    //let url = `http://api.giphy.com/v1/tags/related/${keycode}?&api_key=${GIPHY_KEY}&limit=4`;
+    let response = await fetch(url);
+    let suggestedWord = await response.json();
+    return suggestedWord;
+}
+
+function autocomplete() { // From w3school example adapted to my web page
+    let inp = document.querySelector('#myInput');
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function (e) {
+        var a, b, i, val = this.value;
+        let arr = [];
+        
+        gifoSuggest(val)
+            .then(suggestedWords => {
+
+                for (let i = 0; i < suggestedWords.data.length; i++) {
+                    arr[i] = suggestedWords.data[i].name;
+                }
+
+                /*close any already open lists of autocompleted values*/
+                closeAllLists();
+                if (!val) { return false; }
+                currentFocus = -1;
+
+                /*create a DIV element that will contain the items (values):*/
+                a = document.createElement("DIV");
+                a.setAttribute("id", this.id + "autocomplete-list");
+                a.setAttribute("class", "autocomplete-items");
+
+                /*append the DIV element as a child of the autocomplete container:*/
+                //this.parentNode.appendChild(a);
+                document.querySelector('#append-list').appendChild(a);
+                
+                /*for each item in the array...*/
+                for (i = 0; i < arr.length; i++) {
+                    /*check if the item starts with the same letters as the text field value:*/
+                    if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                        /*create a DIV element for each matching element:*/
+                        b = document.createElement("DIV");
+                        /*make the matching letters bold:*/
+                        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+                        b.innerHTML += arr[i].substr(val.length);
+                        /*insert a input field that will hold the current array item's value:*/
+                        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                        /*execute a function when someone clicks on the item value (DIV element):*/
+                        b.addEventListener("click", function (e) {
+                            /*insert the value for the autocomplete text field:*/
+                            inp.value = this.getElementsByTagName("input")[0].value;
+                            /*close the list of autocompleted values,
+                            (or any other open lists of autocompleted values:*/
+                            closeAllLists();
+                        });
+                        a.appendChild(b);
+                    }
+                }
+            });
+    });
+
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function (e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+            /*If the arrow DOWN key is pressed,
+            increase the currentFocus variable:*/
+            currentFocus++;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 38) { //up
+            /*If the arrow UP key is pressed,
+            decrease the currentFocus variable:*/
+            currentFocus--;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 13) {
+            /*If the ENTER key is pressed, prevent the form from being submitted,*/
+            e.preventDefault();
+            if (currentFocus > -1) {
+                /*and simulate a click on the "active" item:*/
+                if (x) x[currentFocus].click();
+            }
+
+            let keyword = document.querySelector("#myInput").value;
+                gifoSearch(keyword)
+                    .then(gifsPromise => displaySearch(gifsPromise, keyword))
+                    .catch(err => console.log('Error gifs search promise: ' + err));
+        }
+    });
+    function addActive(x) {
+        /*a function to classify an item as "active":*/
+        if (!x) return false;
+        /*start by removing the "active" class on all items:*/
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (x.length - 1);
+        /*add class "autocomplete-active":*/
+        x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+        /*a function to remove the "active" class from all autocomplete items:*/
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+        }
+    }
+    function closeAllLists(elmnt) {
+        /*close all autocomplete lists in the document,
+        except the one passed as an argument:*/
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+}
+
 // MAIN APP FUNCTION
 myApp.EventHandlers = {
     // CLICK EVENTS
@@ -112,10 +237,10 @@ myApp.EventHandlers = {
         document.body.classList.toggle("dark");
     },
     cleanSearch: function () {
-        document.querySelector("#search").value = "";
+        document.querySelector("#myInput").value = "";
     },
     searchGifoSubmitClick: function () {
-        let keyword = document.querySelector("#search").value;
+        let keyword = document.querySelector('#myInput').value;
         gifoSearch(keyword, GIPHY_KEY)
             .then(gifsPromise => displaySearch(gifsPromise, keyword))
             .catch(err => console.log('Error gifs search promise: ' + err));
@@ -126,25 +251,42 @@ myApp.EventHandlers = {
     },
 
     // KEYPRESS EVENTS
-    searchGifoSubmitKeypress: function () {
-        document
-            .querySelector("#search").addEventListener("keypress", function (e) {
-                var keycode = e.code;
-                if (keycode == "Enter") {
-                    let keyword = document.querySelector("#search").value;
-                    gifoSearch(keyword, GIPHY_KEY)
-                        .then(gifsPromise => displaySearch(gifsPromise, keyword))
-                        .catch(err => console.log('Error gifs search promise: ' + err));
-                } else {
-                    //Funcion gifo sugerencias
-                    console.log('Sugerencias...');
-                }
-            });
-    },
+    // searchGifoSubmitKeypress: function () {
+    //     let inputSearch = document.querySelector("#search");
+
+    //     inputSearch.addEventListener("keypress", function (e) {
+    //         var keycode = e.key;
+
+    //         if (keycode == "Enter") { // Search with the keyword typed
+    //             let keyword = document.querySelector("#search").value;
+    //             gifoSearch(keyword)
+    //                 .then(gifsPromise => displaySearch(gifsPromise, keyword))
+    //                 .catch(err => console.log('Error gifs search promise: ' + err));
+    //         } else { // Autocomplete the input text
+    //             let partialKeyword = inputSearch.value;
+    //             ///console.log(partialKeyword);
+    //             gifoSuggest(partialKeyword)
+    //                 .then(suggestedWords => {
+    //                     let gifoSuggestedArray = [];
+    //                     for (let i = 0; i < suggestedWords.data.length; i++) {
+    //                         gifoSuggestedArray[i] = suggestedWords.data[i].name;
+    //                     }
+    //                     //console.log(gifoSuggestedArray);
+    //                     // Usar values para obtener los valores de name
+    //                     autocomplete(inputSearch, gifoSuggestedArray, keycode);
+    //                 });
+    //             autocomplete(inputSearch, gifoSuggestedArray, keycode);
+    //         }
+    //     });
+    //},
 };
 
-myApp.EventHandlers.searchGifoSubmitKeypress();
+//myApp.EventHandlers.searchGifoSubmitKeypress();
 
-gifoTrending(GIPHY_KEY)
+myApp.EventHandlers.cleanSearch();
+
+autocomplete();
+
+gifoTrending()
     .then(gifsPromise => displayTrending(gifsPromise))
     .catch(err => console.log('Error gifs trending promise: ' + err));
